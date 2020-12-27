94e5ddb92ed3563c6a6ca55f8871f14630039fe9
Patrick Lake <plake492@gmail.com>
2020-12-27 15:13:24 -0500

    Add drag and drop to transaction list

--- src/componenets/TableRow.js
+++ src/componenets/TableRow.js
@@ -1,32 +1,53 @@
 import React from 'react'
 import { format } from 'date-fns'
 import { add$ } from '../utils/helpers'
+import { Draggable } from 'react-beautiful-dnd'
+
 function TableRow ({
   transactionArr,
   currentBalance,
   displayLineItem
 }) {
+  const getItemStyle = (isDragging, draggableStyle) => ({
+    userSelect: 'none',
+    padding: '1rem',
+    background: isDragging ? '#e8e8ee' : null,
+    ...draggableStyle
+  })
+
   return (
     <>
-      {transactionArr ? transactionArr.map((item, index) => {
-        add$(item) // Add $ to any number
-        return (
-          <tr
-            style={{ padding: '1rem' }}
-            key={index}
-            className='table_row'
-            id={item._id}
-            onClick={(e) => displayLineItem(e.target.parentElement.id)}
-          >
-            <th>{format(item.date, 'LL/dd')}</th>
-            <td>{item.paidTo}</td>
-            <td>{item.category}</td>
-            <td>{item.withdrawlAmount$}</td>
-            <td>{item.depositAmount$}</td>
-            <td>{item.currentBalance$}</td>
-          </tr>
-        )
-      }) : (<tr><td>no records</td></tr>)}
+      {transactionArr
+        ? transactionArr.map((item, index) => {
+          return (
+            <Draggable key={item._id} draggableId={item._id} index={index}>
+              {(provided, snapshot) => (
+                (
+                  <tr
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    style={getItemStyle(
+                      snapshot.isDragging,
+                      provided.draggableProps.style
+                    )}
+
+                    className='table_row'
+                    id={item._id}
+                    onClick={(e) => displayLineItem(e.target.parentElement.id)}
+                  >
+                    <th>{format(item.date, 'LL/dd')}</th>
+                    <td>{item.paidTo}</td>
+                    <td>{item.category}</td>
+                    <td>{/* add $ */ add$(item.withdrawlAmount)}</td>
+                    <td>{/* add $ */ add$(item.depositAmount)}</td>
+                    <td>{/* add $ */ add$(item.currentBalance)}</td>
+                  </tr>
+                )
+              )}
+            </Draggable>
+          )
+        }) : (<tr><td>no records</td></tr>)}
     </>
   )
 }
